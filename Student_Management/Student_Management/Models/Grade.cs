962e08b0abb1421bde224ab0f98e22143616f1d0
Nguyen Phong Hao <haonphe153346@fpt.edu.vn>
2022-07-04 22:51:28 +0700

    Initial commit

---
+++ Student_Management/Student_Management/Models/Grade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Student_Management.Models
+{
+    public partial class Grade
+    {
+        public int GradeId { get; set; }
+        public int? GradeCategoryId { get; set; }
+        public string GradeName { get; set; }
+
+        public virtual GradeCategory GradeCategory { get; set; }
+    }
+}
---
+++ Student_Management/Student_Management/Models/Slot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Student_Management.Models
+{
+    public partial class Slot
+    {
+        public Slot()
+        {
+            Schedules = new HashSet<Schedule>();
+        }
+
+        public int SlotId { get; set; }
+        public string SlotTime { get; set; }
+
+        public virtual ICollection<Schedule> Schedules { get; set; }
+    }
+}
